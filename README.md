10e787e16f9d96b10cbcbebefa60b5602f0c7f48
sainiii <saini.sunder158@gmail.com>
2020-12-23 22:20:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-embed-video-example-wr3mfh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngx-embed-video-example-wr3mfh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { EmbedVideoService } from 'ngx-embed-video';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Angular 6';
+
+  yt_iframe_html: any;
+  vimeo_iframe_html: any;
+  dm_iframe_html: any;
+  
+  vimeoUrl = "https://vimeo.com/197933516";
+  youtubeUrl = "https://www.youtube.com/watch?v=iHhcHTlGtRs";
+  dailymotionUrl = "https://www.dailymotion.com/video/x20qnej_red-bull-presents-wild-ride-bmx-mtb-dirt_sport";
+
+  constructor(
+    private embedService: EmbedVideoService
+  ) {
+    this.yt_iframe_html = this.embedService.embed(this.youtubeUrl);
+    this.vimeo_iframe_html = this.embedService.embed(this.vimeoUrl);
+    this.dm_iframe_html = this.embedService.embed(this.dailymotionUrl);
+  }
+}
